86acc87f1f11eea792f070f7a2c99846236aaa21
Андрей <murzin.a@recifra.ru>
2025-02-28 15:35:27 +0500

    modal form in work

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,3 @@
-const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-    autoplay: {
-      delay: 3000,
-    },
-    slidesPerView: 3,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
-
-swiper.mousewheel.disable()
+function toggleForm(){
+    alert('hello')
+}
